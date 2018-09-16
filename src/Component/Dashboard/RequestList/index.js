621efeaa703deaacb528ab29807c947349b458c2
maaazahmed <maazahmed2k16@gmail.com>
2018-09-16 14:35:27 +0500

    Updated

--- src/Component/Dashboard/RequestList/index.js
+++ src/Component/Dashboard/RequestList/index.js
@@ -7,8 +7,8 @@ import {
     TouchableOpacity
 } from 'react-native';
 import {
-    Container,
-    Content,
+    Button,
+    Icon,
     Card,
     CardItem,
     Thumbnail,
@@ -36,49 +36,52 @@ export default class RequestList extends Component {
         }
         return (
             <View>
-                <Header />
+                <Header>
+                    <Left>
+                        <Icon name='menu' />
+                    </Left>
+                </Header>
                 <View style={styles.GroupListContainer} >
-                    
-                        <FlatList
-                            onScroll={() => { this.setState({ count: this.state.count + 3 }) }}
-                            data={arr}
-                            renderItem={({ item, index }) =>
+                    <FlatList
+                        onScroll={() => { this.setState({ count: this.state.count + 3 }) }}
+                        data={arr}
+                        renderItem={({ item, index }) =>
                             <Card key={index} style={styles.Card}>
-                            <CardItem>
-                                <Left>
-                                    <Thumbnail source={{ uri: "https://scontent.fkhi4-1.fna.fbcdn.net/v/t1.0-9/22279707_282900065546139_3654734220274300235_n.jpg?_nc_cat=0&_nc_eui2=AeHRdlM8eqDcyXmtWKac6uJUh8LA5ViKlAMp9dWzSSuc7Y8KO6GMgjjKiqR_Gojei24gRRZcq8TTWxqbFjmVGucO2bwtzjEegT2RH5IJSpAMl43qsJPq-u3o65hZI3-Smpk&oh=57a8afb496dcfcca9e8f13ec36bed700&oe=5C2CA1EA" }} />
-                                    <Body>
-                                        <Text>{item.GroupName}</Text>
-                                        <Text note>People</Text>
-                                    </Body>
-                                </Left>
-                                <Right>
-                                    <Body>
-                                        <View style={styles.CardButtun} >
-                                            <TouchableOpacity
-                                               
-                                                activeOpacity={0.8} style={styles.rejectButt}>
-                                                <Text style={styles.rejectText}>
-                                                    Reject
+                                <CardItem>
+                                    <Left>
+                                        <Thumbnail source={{ uri: "https://scontent.fkhi4-1.fna.fbcdn.net/v/t1.0-9/22279707_282900065546139_3654734220274300235_n.jpg?_nc_cat=0&_nc_eui2=AeHRdlM8eqDcyXmtWKac6uJUh8LA5ViKlAMp9dWzSSuc7Y8KO6GMgjjKiqR_Gojei24gRRZcq8TTWxqbFjmVGucO2bwtzjEegT2RH5IJSpAMl43qsJPq-u3o65hZI3-Smpk&oh=57a8afb496dcfcca9e8f13ec36bed700&oe=5C2CA1EA" }} />
+                                        <Body>
+                                            <Text>{item.GroupName}</Text>
+                                            <Text note>People</Text>
+                                        </Body>
+                                    </Left>
+                                    <Right>
+                                        <Body>
+                                            <View style={styles.CardButtun} >
+                                                <TouchableOpacity
+
+                                                    activeOpacity={0.8} style={styles.rejectButt}>
+                                                    <Text style={styles.rejectText}>
+                                                        Reject
                                                 </Text>
-                                            </TouchableOpacity>
-                                            <TouchableOpacity
-                                               
-                                                activeOpacity={0.8}
-                                                style={styles.aprovButt} >
-                                                <Text style={styles.aprovText} >
-                                                    Approve
+                                                </TouchableOpacity>
+                                                <TouchableOpacity
+
+                                                    activeOpacity={0.8}
+                                                    style={styles.aprovButt} >
+                                                    <Text style={styles.aprovText} >
+                                                        Approve
                                          </Text>
-                                            </TouchableOpacity>
-                                        </View>
-                                    </Body>
-                                </Right>
-                            </CardItem>
-                        </Card>
-                            }
-                            keyExtractor={(item) => { return item.key }}
-                        />
-         
+                                                </TouchableOpacity>
+                                            </View>
+                                        </Body>
+                                    </Right>
+                                </CardItem>
+                            </Card>
+                        }
+                        keyExtractor={(item) => { return item.key }}
+                    />
+
                 </View>
             </View>
 
@@ -131,8 +134,8 @@ const styles = StyleSheet.create({
         width: "50%",
         justifyContent: "center",
         alignItems: "center",
-        borderWidth:1,
-        borderColor:"#3f51b5"
+        borderWidth: 1,
+        borderColor: "#3f51b5"
     },
     rejectText: {
         color: "#3f51b5"
